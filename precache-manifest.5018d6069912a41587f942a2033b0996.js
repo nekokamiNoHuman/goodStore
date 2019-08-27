9e6117738c290dde24d09c29fef3e708080430c5
nekokamiNoHuman <homosapiensaf@gmail.com>
2019-08-28 01:21:31 +0800

    Updates

---
+++ precache-manifest.5018d6069912a41587f942a2033b0996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d493a2bc56f4c2ace47a1bb631b093e",
+    "url": "/goodStore/index.html"
+  },
+  {
+    "revision": "249340d434d270be5d40",
+    "url": "/goodStore/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "6f831edac82bd4fe4bea",
+    "url": "/goodStore/static/js/2.28534e15.chunk.js"
+  },
+  {
+    "revision": "249340d434d270be5d40",
+    "url": "/goodStore/static/js/main.52de7e37.chunk.js"
+  },
+  {
+    "revision": "7353a9c87e95102bc526",
+    "url": "/goodStore/static/js/runtime~main.b1129cef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goodStore/static/media/logo.5d5d9eef.svg"
+  }
+]);
